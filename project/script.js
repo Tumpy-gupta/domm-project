c682b4f2c66a1286bddf562cb873c8ae7f199259
unknown <tumpygupta2013@gmail.com>
2024-01-12 21:01:41 +0530

    added

---
+++ project/script.js
@@ -0,0 +1,8 @@
+let a = document.getElementsByClassName("box");
+a[0].style.color = "blue";
+a[0].style.backgroundColor = "yellow";
+a[0].style.width = "22%";
+a[0].style.height = "13rem";
+a[0].style.padding = "2px";
+a[0].style.paddingLeft = "15px";
+a[0].style.margin = "20px";
